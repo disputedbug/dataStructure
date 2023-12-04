31afbbaf73b221b696d17340d82c9ed7b233c279
Himanshu Sharma <disputedbug@gmail.com>
2023-12-04 15:05:33 +0530

    Adding iterative bnary serach program

---
+++ 4_searching/iterativeBinSearch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int binSearch(int arr[], int len, int item)
+{
+    int start = 0;
+    int end = len-1;
+    int mid = 0;
+
+    while (start <= end)
+    {
+        mid = (start+end)/2;
+
+        if (arr[mid] == item)
+        {
+            return mid;
+        }
+        else if (arr[mid] > item)
+        {
+            end = mid-1;
+        }
+        else
+        {
+            start = mid+1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60};
+    int item = 40;
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    int ret = binSearch(arr, len, item);
+    cout << "Element found at: " << ret << endl;
+}
