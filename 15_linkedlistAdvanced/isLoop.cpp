9fa2a2f646cd9e3e3dfe12143191ad4a0050f194
Himanshu Sharma <disputedbug@gmail.com>
2023-12-18 00:47:27 +0530

    Program to find if there is a loop in the LL

---
+++ 15_linkedlistAdvanced/isLoop.cpp
@@ -0,0 +1,73 @@
+// Program to find if there is a loop in the LL
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int item;
+    Node* next;
+
+    Node(int x)
+    {
+        item = x;
+        next = NULL;
+    }
+};
+
+bool isLoop(Node* head)
+{
+    Node* fast = head;
+    Node* slow = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+Node* endNode;
+Node* midNode;
+Node* insertAtEnd(Node* head, int num)
+{
+    Node* temp = new Node(num);
+    Node* curr = head;
+
+    if (head == NULL)
+    {
+        endNode = temp;
+        return temp;
+    }
+
+    if (num == 110)
+    {
+        midNode = temp;
+    }
+    if (num == 200)
+    {
+        temp = midNode;
+    }
+    endNode->next = temp;
+    endNode = temp; // comment this line to remove the loop
+
+    return head;
+}
+
+int main()
+{
+    Node* head = NULL;
+    for (int i = 1; i <= 20; i++)
+    {
+        head = insertAtEnd(head, i*10);
+    }
+
+    bool ret  = isLoop(head);
+
+    cout << boolalpha << "Loop in the LL found: " << ret << endl;
+}
